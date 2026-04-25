e95fd5c9bfeb70fa0b8573a0919384ae99061bd7
Ivanilton Polato <ipolato@gmail.com>
2026-04-25 13:41:10 -0300

    fix: add missing repositories to SeedModule to resolve UnknownDependenciesException

--- api/src/seed/seed.module.ts
+++ api/src/seed/seed.module.ts
@@ -5,9 +5,11 @@ import { Usuario } from '../modules/entities/usuario.entity';
 import { Curso } from '../modules/entities/curso.entity';
 import { Local } from '../modules/entities/local.entity';
 import { Equipamento } from '../modules/entities/equipamento.entity';
+import { Disciplina } from '../modules/entities/disciplina.entity';
+import { Aula } from '../modules/entities/aula.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario, Curso, Local, Equipamento])],
+  imports: [TypeOrmModule.forFeature([Usuario, Curso, Local, Equipamento, Disciplina, Aula])],
   providers: [SeedService],
 })
 export class SeedModule {}
